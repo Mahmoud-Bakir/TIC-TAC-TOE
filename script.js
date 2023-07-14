5019a37ae5d7257890df07dc578c491c67301b5a
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-07-14 15:49:08 +0300

    More elements are assigned to variables

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ window.onload=function(){
     let col3_1 = document.getElementById("col3_1");
     let col3_2 = document.getElementById("col3_2");
     let col3_3 = document.getElementById("col3_3");
+    let btn1=document.getElementById("btn1");
+    let btn2=document.getElementById("btn2");
+    let player_1=document.getElementById("player_1");
+    let player_2=document.getElementById("player_2");
+
     console.log(col1_1);
     col1_1.setAttribute("class","col_X");
     col1_2.setAttribute("class","col_Y");
-
-    
 }
