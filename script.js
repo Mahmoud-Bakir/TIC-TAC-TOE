edf9a4755bf3ac936e3b87f97b7c82273d0378f0
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-07-14 15:38:44 +0300

    Variables in script are assigned

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 window.onload=function(){
     let col1_1 = document.getElementById("col1_1");
+    let col1_2 = document.getElementById("col1_2");
+    let col1_3 = document.getElementById("col1_3");
+    let col2_1 = document.getElementById("col2_1");
+    let col2_2 = document.getElementById("col2_2");
+    let col2_3 = document.getElementById("col2_3");
+    let col3_1 = document.getElementById("col3_1");
+    let col3_2 = document.getElementById("col3_2");
+    let col3_3 = document.getElementById("col3_3");
     console.log(col1_1);
     col1_1.setAttribute("class","col_X");
+    col1_2.setAttribute("class","col_Y");
+
+    
 }
