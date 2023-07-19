5811d48f4362e145c068d91cc9198fa26ed20213
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-07-20 00:43:53 +0300

    Play button added

--- script.js
+++ script.js
@@ -10,10 +10,32 @@ window.onload=function(){
     let col3_3 = document.getElementById("col3_3");
     let btn1=document.getElementById("btn1");
     let btn2=document.getElementById("btn2");
-    let player_1=document.getElementById("player_1");
-    let player_2=document.getElementById("player_2");
-
+    let first=false;
+    let second=false;
     console.log(col1_1);
     col1_1.setAttribute("class","col_X");
     col1_2.setAttribute("class","col_Y");
+
+    btn1.addEventListener("click",function(){
+        let player_1=document.getElementById("player_1");
+        if(player_1.value.trim() !=""){
+           console.log(player_1.value);
+           first=true;
+           if(first==true && second ==true){
+            ;
+        }}
+      })
+    btn2.addEventListener("click",function(){
+        let player_2=document.getElementById("player_2");
+        if(player_2.value.trim() !=""){
+            console.log(player_2.value);
+            second=true;
+            if(first==true && second ==true){
+                console.log("Hello");
+            }}
+           })
+
+    if(first==true && second ==true){
+        console.log("Hello");
+    }
 }
