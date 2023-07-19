792b8c0f56b9ee9a7906785c6b0492838921a4e8
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-07-20 01:39:35 +0300

    X_O winning logic added

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ window.onload=function(){
     let play=document.getElementById("play");
     let first=false;
     let second=false;
+    let x=[10,11,12,13,14,15,16,17,18]
+    let y=[10,11,12,13,14,15,16,17,18]
     console.log(col1_1);
     let count=2;
 
@@ -22,6 +24,7 @@ window.onload=function(){
         if(player_1.value.trim() !=""){
            console.log(player_1.value);
            first=true;
+         
           }
       })
     btn2.addEventListener("click",function(){
@@ -38,90 +41,160 @@ window.onload=function(){
                 if(count%2==0){
                     col1_1.setAttribute("class","col_X")
                     count ++
+                    x[0]=0;
+                    if((x[0]== x[1] && x[1]==x[2])||(x[0]== x[3] && x[3]==x[6])||(x[6]== x[7] && x[7]==x[8])||(x[2]== x[5] && x[5]==x[8])||(x[3]== x[4] && x[4]==x[5])||(x[1]== x[4] && x[4]==x[7])||(x[0]== x[4] && x[4]==x[5])||(x[2]== x[4] && x[4]==x[6]))
+                    console.log("HOOOHOOHOH")
                 }
                 else{
                     col1_1.setAttribute("class","col_Y")
                     count++
+                    y[0]=1
+                    if((y[0]== y[1] && y[1]==y[2])||(y[0]== y[3] && y[3]==y[6])||(y[6]== y[7] && y[7]==y[8])||(y[2]== y[5] && y[5]==y[8])||(y[3]== y[4] && y[4]==y[5])||(y[1]== y[4] && y[4]==y[7])||(y[0]== y[4] && y[4]==y[5])||(y[2]== y[4] && y[4]==y[6]))
+                    console.log("HOOOHOOHOH")
                 }
             })
             col1_2.addEventListener("click",function(){
                 if(count%2==0){
                     col1_2.setAttribute("class","col_X")
                     count ++
+                    x[1]=0;
+                    if((x[0]== x[1] && x[1]==x[2])||(x[0]== x[3] && x[3]==x[6])||(x[6]== x[7] && x[7]==x[8])||(x[2]== x[5] && x[5]==x[8])||(x[3]== x[4] && x[4]==x[5])||(x[1]== x[4] && x[4]==x[7])||(x[0]== x[4] && x[4]==x[5])||(x[2]== x[4] && x[4]==x[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
                 else{
                     col1_2.setAttribute("class","col_Y")
                     count++
+                    y[1]=1
+                    if((y[0]== y[1] && y[1]==y[2])||(y[0]== y[3] && y[3]==y[6])||(y[6]== y[7] && y[7]==y[8])||(y[2]== y[5] && y[5]==y[8])||(y[3]== y[4] && y[4]==y[5])||(y[1]== y[4] && y[4]==y[7])||(y[0]== y[4] && y[4]==y[5])||(y[2]== y[4] && y[4]==y[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
             })
             col1_3.addEventListener("click",function(){
                 if(count%2==0){
                     col1_3.setAttribute("class","col_X")
                     count ++
+                    x[2]=0;
+                    if((x[0]== x[1] && x[1]==x[2])||(x[0]== x[3] && x[3]==x[6])||(x[6]== x[7] && x[7]==x[8])||(x[2]== x[5] && x[5]==x[8])||(x[3]== x[4] && x[4]==x[5])||(x[1]== x[4] && x[4]==x[7])||(x[0]== x[4] && x[4]==x[5])||(x[2]== x[4] && x[4]==x[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
                 else{
                     col1_3.setAttribute("class","col_Y")
                     count++
+                    y[0]=1
+                    if((y[0]== y[1] && y[1]==y[2])||(y[0]== y[3] && y[3]==y[6])||(y[6]== y[7] && y[7]==y[8])||(y[2]== y[5] && y[5]==y[8])||(y[3]== y[4] && y[4]==y[5])||(y[1]== y[4] && y[4]==y[7])||(y[0]== y[4] && y[4]==y[5])||(y[2]== y[4] && y[4]==y[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
             })
             col2_1.addEventListener("click",function(){
                 if(count%2==0){
                     col2_1.setAttribute("class","col_X")
                     count ++
+                    x[3]=0;
+                    if((x[0]== x[1] && x[1]==x[2])||(x[0]== x[3] && x[3]==x[6])||(x[6]== x[7] && x[7]==x[8])||(x[2]== x[5] && x[5]==x[8])||(x[3]== x[4] && x[4]==x[5])||(x[1]== x[4] && x[4]==x[7])||(x[0]== x[4] && x[4]==x[5])||(x[2]== x[4] && x[4]==x[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
                 else{
                     col2_1.setAttribute("class","col_Y")
                     count++
+                    y[0]=1
+                    if((y[0]== y[1] && y[1]==y[2])||(y[0]== y[3] && y[3]==y[6])||(y[6]== y[7] && y[7]==y[8])||(y[2]== y[5] && y[5]==y[8])||(y[3]== y[4] && y[4]==y[5])||(y[1]== y[4] && y[4]==y[7])||(y[0]== y[4] && y[4]==y[5])||(y[2]== y[4] && y[4]==y[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
             })
             col2_2.addEventListener("click",function(){
                 if(count%2==0){
                     col2_2.setAttribute("class","col_X")
                     count ++
+                    x[4]=0;
+                    if((x[0]== x[1] && x[1]==x[2])||(x[0]== x[3] && x[3]==x[6])||(x[6]== x[7] && x[7]==x[8])||(x[2]== x[5] && x[5]==x[8])||(x[3]== x[4] && x[4]==x[5])||(x[1]== x[4] && x[4]==x[7])||(x[0]== x[4] && x[4]==x[5])||(x[2]== x[4] && x[4]==x[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
                 else{
                     col2_2.setAttribute("class","col_Y")
                     count++
+                    y[0]=1
+                    if((y[0]== y[1] && y[1]==y[2])||(y[0]== y[3] && y[3]==y[6])||(y[6]== y[7] && y[7]==y[8])||(y[2]== y[5] && y[5]==y[8])||(y[3]== y[4] && y[4]==y[5])||(y[1]== y[4] && y[4]==y[7])||(y[0]== y[4] && y[4]==y[5])||(y[2]== y[4] && y[4]==y[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
             })
             col2_3.addEventListener("click",function(){
                 if(count%2==0){
                     col2_3.setAttribute("class","col_X")
                     count ++
+                    x[5]=0;
+                    if((x[0]== x[1] && x[1]==x[2])||(x[0]== x[3] && x[3]==x[6])||(x[6]== x[7] && x[7]==x[8])||(x[2]== x[5] && x[5]==x[8])||(x[3]== x[4] && x[4]==x[5])||(x[1]== x[4] && x[4]==x[7])||(x[0]== x[4] && x[4]==x[5])||(x[2]== x[4] && x[4]==x[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
                 else{
                     col2_3.setAttribute("class","col_Y")
                     count++
+                    y[0]=1
+                    if((y[0]== y[1] && y[1]==y[2])||(y[0]== y[3] && y[3]==y[6])||(y[6]== y[7] && y[7]==y[8])||(y[2]== y[5] && y[5]==y[8])||(y[3]== y[4] && y[4]==y[5])||(y[1]== y[4] && y[4]==y[7])||(y[0]== y[4] && y[4]==y[5])||(y[2]== y[4] && y[4]==y[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
             })
             col3_1.addEventListener("click",function(){
                 if(count%2==0){
                     col3_1.setAttribute("class","col_X")
                     count ++
+                    x[6]=0;
+                    if((x[0]== x[1] && x[1]==x[2])||(x[0]== x[3] && x[3]==x[6])||(x[6]== x[7] && x[7]==x[8])||(x[2]== x[5] && x[5]==x[8])||(x[3]== x[4] && x[4]==x[5])||(x[1]== x[4] && x[4]==x[7])||(x[0]== x[4] && x[4]==x[5])||(x[2]== x[4] && x[4]==x[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
                 else{
                     col3_1.setAttribute("class","col_Y")
                     count++
+                    y[0]=1
+                    if((y[0]== y[1] && y[1]==y[2])||(y[0]== y[3] && y[3]==y[6])||(y[6]== y[7] && y[7]==y[8])||(y[2]== y[5] && y[5]==y[8])||(y[3]== y[4] && y[4]==y[5])||(y[1]== y[4] && y[4]==y[7])||(y[0]== y[4] && y[4]==y[5])||(y[2]== y[4] && y[4]==y[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
             })
             col3_2.addEventListener("click",function(){
                 if(count%2==0){
                     col3_2.setAttribute("class","col_X")
                     count ++
+                    x[7]=0;
+                    if((x[0]== x[1] && x[1]==x[2])||(x[0]== x[3] && x[3]==x[6])||(x[6]== x[7] && x[7]==x[8])||(x[2]== x[5] && x[5]==x[8])||(x[3]== x[4] && x[4]==x[5])||(x[1]== x[4] && x[4]==x[7])||(x[0]== x[4] && x[4]==x[5])||(x[2]== x[4] && x[4]==x[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
                 else{
                     col3_2.setAttribute("class","col_Y")
                     count++
+                    y[0]=1
+                    if((y[0]== y[1] && y[1]==y[2])||(y[0]== y[3] && y[3]==y[6])||(y[6]== y[7] && y[7]==y[8])||(y[2]== y[5] && y[5]==y[8])||(y[3]== y[4] && y[4]==y[5])||(y[1]== y[4] && y[4]==y[7])||(y[0]== y[4] && y[4]==y[5])||(y[2]== y[4] && y[4]==y[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
             })
             col3_3.addEventListener("click",function(){
                 if(count%2==0){
                     col3_3.setAttribute("class","col_X")
                     count ++
+                    x[8]=0;
+                    if((x[0]== x[1] && x[1]==x[2])||(x[0]== x[3] && x[3]==x[6])||(x[6]== x[7] && x[7]==x[8])||(x[2]== x[5] && x[5]==x[8])||(x[3]== x[4] && x[4]==x[5])||(x[1]== x[4] && x[4]==x[7])||(x[0]== x[4] && x[4]==x[5])||(x[2]== x[4] && x[4]==x[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
                 else{
                     col3_3.setAttribute("class","col_Y")
                     count++
+                    y[0]=1
+                    if((y[0]== y[1] && y[1]==y[2])||(y[0]== y[3] && y[3]==y[6])||(y[6]== y[7] && y[7]==y[8])||(y[2]== y[5] && y[5]==y[8])||(y[3]== y[4] && y[4]==y[5])||(y[1]== y[4] && y[4]==y[7])||(y[0]== y[4] && y[4]==y[5])||(y[2]== y[4] && y[4]==y[6]))
+                    console.log("HOOOHOOHOH")
+
                 }
             })
         }
